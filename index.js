26a2142008b2d358a401a5045cb56c008ee7f277
Kharat-Vaishali <kharatvaishali786@gmail.com>
2025-07-04 00:52:02 -1200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Git_Crash_Course ')
+
+const userName = 'Vaishali'
+console.log(userName)
+
+
+const userAge = 23
+console.log(userAge)
+let total = 200000
+function sayHii(){
+    console.log('Helllo')
+}
+sayHii()
+
