11c4643d3de383105a9573b852e939a20de6c064
Kharat-Vaishali <kharatvaishali786@gmail.com>
2025-07-04 00:59:52 -1200

    initial commit

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const userName = 'Vaishali'
 console.log(userName)
 
 
-const userAge = 23
-console.log(userAge)
+const userAge1 = 23
+console.log(userAge1)
 let total = 200000
 function sayHii(){
     console.log('Helllo')
 }
 sayHii()
 
+Promise.resolve().then(()=>{
+    console.log('Promise')
+},1000)
+
+
+let result =((a,b)=>{
+    setTimeout(()=>{
+        console.log(a + b)
+    },2000)
+})
+result(12,45)
