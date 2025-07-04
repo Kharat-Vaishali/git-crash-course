086772f78aa2cc90fd1f9326a56304f4ee2f0804
Kharat-Vaishali <kharatvaishali786@gmail.com>
2025-07-04 07:07:19 -1200

    new changes add

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
-// function sayHello(){
-//     console.log('Hello')
-// }
-// sayHello()
+function sayHello(){
+    console.log('Hello')
+}
+sayHello()
 
 
 let total = 0
@@ -16,3 +16,15 @@ console.log(myFunction(50))
 
 
 console.log('Hello')
+
+
+
+
+function myFunction(msg,callBack){
+console.log('hello' , msg)
+callBack()
+}
+function sayHii(){
+    console.log('Hello')
+}
+myFunction('vaishali',sayHii)
